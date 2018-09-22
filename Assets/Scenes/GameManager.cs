13d98e6f5b0bfc8b65e94f752cbd61adc1c52a0d
Wi <dnltjdghks1@gmail.com>
2018-09-22 16:47:36 +0900

    PingpongProject

---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    [SerializeField]
+    Ball ball;
+    [SerializeField]
+    Padle padle;
+    public static Vector2 bottomLeft;
+    public static Vector2 topRight;
+    // Use this for initialization
+    void Start()
+    {
+        // convert screen's pixel cordinate into game's cordinate ( in this case 0, 0 ) 
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+
+        Instantiate(ball);
+
+        Padle padle1 = Instantiate(padle) as Padle;
+        Padle padle2 = Instantiate(padle) as Padle;
+        padle1.Init(true); // right padle 
+        padle2.Init(false); // left padle 
+
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scenes/Padle.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Padle : MonoBehaviour {
+    [SerializeField]
+    float speed;
+
+    float height;
+
+    string input;
+    bool isRight; 
+	// Use this for initialization
+	void Start () {
+        height = transform.localScale.y;
+        speed = 10f;
+
+	}
+    public void Init(bool isRightPadle){
+
+        isRight = isRightPadle;
+
+        Vector2 pos = Vector2.zero;
+
+        if(isRightPadle){
+            // Place paddle on the right of screen // 오른쪽
+            pos = new Vector2(GameManager.topRight.x, 0);
+            pos -= Vector2.right * transform.localScale.x;
+
+            input = "PadleRight";
+        }else{
+            // Place paddle on the left of screen // 왼쪽 
+            pos = new Vector2(GameManager.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x;
+            input = "PadleLeft";
+        }
+        // Update this paddle's  positon
+        transform.position = pos;
+        transform.name = input;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+        if(transform.position.y < GameManager.bottomLeft.y + height / 2 && move < 0){
+            move = 0;
+        }
+        if (transform.position.y > GameManager.topRight.y - height / 2 && move > 0)
+        {
+            move = 0;
+        }
+        transform.Translate(move * Vector2.up);
+    }
+
+}
